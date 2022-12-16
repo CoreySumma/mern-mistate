3dc99c489e0101d6a16eea23beb88d5e400c0b5e
CoreySumma <CoreySumma@gmail.com>
2022-12-16 13:40:14 -0600

    sort & display entries by created at day

--- src/components/EntryCard/EntryCard.jsx
+++ src/components/EntryCard/EntryCard.jsx
@@ -1,9 +1,18 @@
-export default function EntryCard({entry}) {
-  const date = new Date(entry.createdAt);
-  console.log(date.toString().substring(0,2))
+export default function EntryCard({ entry }) {
+  
+  const days = {
+    0: 'sunday',
+    1: 'monday',
+    2: 'tuesday',
+    3: 'wednesday',
+    4: 'thursday',
+    5: 'friday',
+    6: 'saturday',
+  }
+
   return (
     <div>
       <p>{entry.emotion}{entry.title}</p>
-    </div> 
+    </div>
   )
 }
--- src/pages/EntryHistoryPage/EntryHistoryPage.jsx
+++ src/pages/EntryHistoryPage/EntryHistoryPage.jsx
@@ -1,31 +1,72 @@
 import EntryCard from "../../components/EntryCard/EntryCard"
+import { useState, useEffect } from "react";
 
 export default function EntryHistoryPage({ entries }) {
-  return (
-  <>
+  const [monday, setMonday] = useState([]);
+  const [tuesday, setTuesday] = useState([]);
+  const [wednesday, setWednesday] = useState([]);
+  const [thursday, setThursday] = useState([]);
+  const [friday, setFriday] = useState([]);
+  const [saturday, setSaturday] = useState([]);
+  const [sunday, setSunday] = useState([]);
+
+    useEffect(() => {
+      function sortEntries() {
+        for (let i = 0; i < 7; i++) {
+          const sortedEntries = entries.filter(function(entry) {
+            const date = new Date(entry.createdAt);
+            const day = date.getDay()
+            if (day === i) return entry;
+          })
+          if (i === 0) setSunday(sortedEntries)
+          if (i === 1) setMonday(sortedEntries)
+          if (i === 2) setTuesday(sortedEntries)
+          if (i === 3) setWednesday(sortedEntries)
+          if (i === 4) setThursday(sortedEntries)
+          if (i === 5) setFriday(sortedEntries)
+          if (i === 6) setSaturday(sortedEntries)
+        }
+      }
+      sortEntries();
+    }, [])
+
+  
+
+    return (
+    <>
       <h1>My Week So Far</h1>
       <div>
-      {entries.map((entry, idx) => (
-        <EntryCard entry={entry} key={idx} />
-      ))}
+        {/* {entries.map((entry, idx) => (
+          <EntryCard entry={entry} key={idx} />
+        ))} */}
+        <br />
+        <br />
+        <hr />
+        <div>
+        <h1>Monday</h1>
+        {monday.length > 0 ? monday.map((entry, idx) => (<EntryCard entry={entry} key={idx}/>)): <h1>No Entry For This Day</h1>} 
+        </div>
+        <hr />
+        <h1>Tuesday</h1>
+        {tuesday.length > 0 ? tuesday.map((entry, idx) => (<EntryCard entry={entry} key={idx}/>)): <h1>No Entry For This Day</h1>} 
+        <hr />
+        <h1>Wednesday</h1>
+        {wednesday.length > 0 ? wednesday.map((entry, idx) => (<EntryCard entry={entry} key={idx}/>)): <h1>No Entry For This Day</h1>} 
+        <hr />
+        <h1>Thursday</h1>
+        {thursday.length > 0 ? thursday.map((entry, idx) => (<EntryCard entry={entry} key={idx}/>)): <h1>No Entry For This Day</h1>} 
+        <hr />
+        <h1>Friday</h1>
+        {friday.length > 0 ? friday.map((entry, idx) => (<EntryCard entry={entry} key={idx}/>)): <h1>No Entry For This Day</h1>} 
+        <hr />
+        <h1>Saturday</h1>
+        {saturday.length > 0 ? saturday.map((entry, idx) => (<EntryCard entry={entry} key={idx}/>)): <h1>No Entry For This Day</h1>} 
+        <hr />
+        <h1>Sunday</h1>
+        {sunday.length > 0 ? sunday.map((entry, idx) => (<EntryCard entry={entry} key={idx}/>)): <h1>No Entry For This Day</h1>} 
+        <hr />
       </div>
-      <br />
-      <br />
-      <hr />
-      <h1>Monday</h1>
-      <hr />
-      <h1>Tuesday</h1>
-      <hr />
-      <h1>Wednesday</h1>
-      <hr />
-      <h1>Thursday</h1>
-      <hr />
-      <h1>Friday</h1>
-      <hr />
-      <h1>Saturday</h1>
-      <hr />
-      <h1>Sunday</h1>
-      <hr />
+
     </>
-  );
+    );
 }
