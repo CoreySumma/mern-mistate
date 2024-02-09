15ae80c2cbc741cf20357b5e75fa89e3a8aac6dd
CoreySumma <CoreySumma@gmail.com>
2024-02-09 17:57:39 -0600

    averaged emotional state by day

--- src/pages/EntryHistoryPage/EntryHistoryPage.jsx
+++ src/pages/EntryHistoryPage/EntryHistoryPage.jsx
@@ -7,6 +7,7 @@ import convertToEmoji from "../../utilities/num-convert-emotion";
 
 export default function EntryHistoryPage({ entries, handleDeleteAll }) {
 
+  // State for each entry for each day of the week
   const [monday, setMonday] = useState([]);
   const [tuesday, setTuesday] = useState([]);
   const [wednesday, setWednesday] = useState([]);
@@ -14,6 +15,15 @@ export default function EntryHistoryPage({ entries, handleDeleteAll }) {
   const [friday, setFriday] = useState([]);
   const [saturday, setSaturday] = useState([]);
   const [sunday, setSunday] = useState([]);
+
+  // State for the average emotion for each day of the week
+  const [mondayAverage, setMondayAverage] = useState([]);
+  const [tuesdayAverage, setTuesdayAverage] = useState([]);
+  const [wednesdayAverage, setWednesdayAverage] = useState([]);
+  const [thursdaAverage, setThursdayAverage] = useState([]);
+  const [fridayAverage, setFridayAverage] = useState([]);
+  const [saturdayAverage, setSaturdayAverage] = useState([]);
+  const [sundayAverage, setSundayAverage] = useState([]);
 
   const [updatedAverageEmotion, setUpdatedAverageEmotion] = useState([]);
 
@@ -35,6 +45,13 @@ export default function EntryHistoryPage({ entries, handleDeleteAll }) {
         if (i === 5) setFriday(sortedEntries);
         if (i === 6) setSaturday(sortedEntries);
       }
+      setMondayAverage(convertToEmoji(averageEmotion(monday)));
+      setTuesdayAverage(convertToEmoji(averageEmotion(tuesday)));
+      setWednesdayAverage(convertToEmoji(averageEmotion(wednesday)));
+      setThursdayAverage(convertToEmoji(averageEmotion(thursday)));
+      setFridayAverage(convertToEmoji(averageEmotion(friday)));
+      setSaturdayAverage(convertToEmoji(averageEmotion(saturday)));
+      setSundayAverage(convertToEmoji(averageEmotion(sunday)));
     }
     sortAndAverageEntries();
   }, [entries])
@@ -51,37 +68,37 @@ export default function EntryHistoryPage({ entries, handleDeleteAll }) {
          <p>(This button WILL delete all entries.)</p>
         <hr />
         <MDBTypography tag='div' className='display-5 pb-3 mb-3 border-bottom'>
-          <h1>Monday</h1>
+          <h1>Monday {mondayAverage}</h1>
           <div style={monday.length > 0 ? { border: "1px solid rgb(220, 219, 220)" } : null} />
           {monday.length > 0 ? monday.map((entry, idx) => (<EntryCard entry={entry} key={idx} />)) : <h1>No Entry For This Day</h1>}
         </MDBTypography>
         <MDBTypography tag='div' className='display-5 pb-3 mb-3 border-bottom'>
-          <h1>Tuesday</h1>
+          <h1>Tuesday {tuesdayAverage}</h1>
           <div style={tuesday.length > 0 ? { border: "1px solid rgb(220, 219, 220)" } : null} />
           {tuesday.length > 0 ? tuesday.map((entry, idx) => (<EntryCard entry={entry} key={idx} />)) : <h1>No Entry For This Day</h1>}
         </MDBTypography>
         <MDBTypography tag='div' className='display-5 pb-3 mb-3 border-bottom'>
-          <h1>Wednesday</h1>
+          <h1>Wednesday {wednesdayAverage}</h1>
           <div style={wednesday.length > 0 ? { border: "1px solid rgb(220, 219, 220)" } : null} />
           {wednesday.length > 0 ? wednesday.map((entry, idx) => (<EntryCard entry={entry} key={idx} />)) : <h1>No Entry For This Day</h1>}
         </MDBTypography>
         <MDBTypography tag='div' className='display-5 pb-3 mb-3 border-bottom'>
-          <h1>Thursday</h1>
+          <h1>Thursday {thursdaAverage}</h1>
           <div style={thursday.length > 0 ? { border: "1px solid rgb(220, 219, 220)" } : null} />
           {thursday.length > 0 ? thursday.map((entry, idx) => (<EntryCard entry={entry} key={idx} />)) : <h1>No Entry For This Day</h1>}
         </MDBTypography>
         <MDBTypography tag='div' className='display-5 pb-3 mb-3 border-bottom'>
-          <h1>Friday</h1>
+          <h1>Friday {fridayAverage}</h1>
           <div style={friday.length > 0 ? { border: "1px solid rgb(220, 219, 220)" } : null} />
           {friday.length > 0 ? friday.map((entry, idx) => (<EntryCard entry={entry} key={idx} />)) : <h1>No Entry For This Day</h1>}
         </MDBTypography>
         <MDBTypography tag='div' className='display-5 pb-3 mb-3 border-bottom'>
-          <h1>Saturday</h1>
+          <h1>Saturday {saturdayAverage}</h1>
           <div style={saturday.length > 0 ? { border: "1px solid rgb(220, 219, 220)" } : null} />
           {saturday.length > 0 ? saturday.map((entry, idx) => (<EntryCard entry={entry} key={idx} />)) : <h1>No Entry For This Day</h1>}
         </MDBTypography>
         <MDBTypography tag='div' className='display-5 pb-3 mb-3 border-bottom'>
-          <h1>Sunday</h1>
+          <h1>Sunday {sundayAverage}</h1>
           <div style={sunday.length > 0 ? { border: "1px solid rgb(220, 219, 220)" } : null} />
           {sunday.length > 0 ? sunday.map((entry, idx) => (<EntryCard entry={entry} key={idx} />)) : <h1>No Entry For This Day</h1>}
         </MDBTypography>
