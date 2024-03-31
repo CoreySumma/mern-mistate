01c81469d00afe8317185d63e7420766f3f917f4
CoreySumma <CoreySumma@gmail.com>
2024-03-31 13:49:30 -0500

    commit before framer motion animations

--- src/components/EntryCard/EntryCard.jsx
+++ src/components/EntryCard/EntryCard.jsx
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom'
+import {motion, AnimatePresence} from 'framer-motion'
+import { useState } from 'react'
 import './EntryCard.css'
 
 export default function EntryCard({ entry }) {
-
+  const [selectedId, setSelectedId] = useState(null)
   return (
       <Link to= {`/entries/${entry._id}`}>
     <div>
