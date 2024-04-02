fa5fc593931328cbf5ca5b952522fb64b1fccb20
CoreySumma <CoreySumma@gmail.com>
2024-04-02 00:43:44 -0500

    complete overhaul of landing page

--- src/components/NavBar/Banner.jsx
+++ src/components/NavBar/Banner.jsx
@@ -4,7 +4,7 @@ import './Banner.css'
 export default function Banner() {
   return (
     <div className="banner">
-      MiState
+      <span className="banner-text">MiState</span>
     </div>
   )
 };
