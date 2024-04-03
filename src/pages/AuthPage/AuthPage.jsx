4691b9246d1b15395a2723f2dab0beca2bb9a6fd
CoreySumma <CoreySumma@gmail.com>
2024-04-03 15:10:08 -0500

    added switch to auth page for login or sign up forms to appear

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,21 +1,31 @@
 import { useState } from "react";
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
 import LoginForm from "../../components/LoginForm/LoginForm";
+import { motion } from "framer-motion";
+import "./AuthPage.css";
 
 export default function AuthPage({ setUser }) {
-  const [showSignUp, setShowSignUp] = useState(false);
+  const spring = {
+    type: "spring",
+    damping: 20,
+    stiffness: 300,
+  };
+
+  const [isOn, setIsOn] = useState(false);
+  const toggleSwitch = () => setIsOn(!isOn);
   return (
     <main>
       <div className="banner">
         <div className="banner-text">MiState</div>
-        </div>
+      </div>
       <div className="page-container">
-        <div>
-          <button onClick={() => setShowSignUp(!showSignUp)}>
-            {showSignUp ? "Log In" : "Sign Up"}
-          </button>
+        <h1>{isOn ? "Sign Up" : "Log In"}</h1>
+        <div className="button-container">
+          <div className="switch" data-isOn={isOn} onClick={toggleSwitch}>
+            <motion.div className="handle" layout transition={spring} />
+          </div>
         </div>
-        {showSignUp ? (
+        {isOn ? (
           <SignUpForm setUser={setUser} />
         ) : (
           <LoginForm setUser={setUser} />
